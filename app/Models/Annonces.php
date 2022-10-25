2c80a1a0ddb8992c6b4c4294b47dd14fa5730790
Murasakibara <joackimcoulibaly5@gmail.com>
2022-10-25 12:28:39 +0000

    atributs du model annonces

--- app/Models/Annonces.php
+++ app/Models/Annonces.php
@@ -2,10 +2,45 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
+use App\Models\Ville;
+use App\Models\SousCategorie;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Annonces extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'id',
+        'titre',
+        'description',
+        'contactWhatsapp',
+        'prix',
+        'type',
+        'Lieu',
+        'email',
+        'photo',
+        'slug',
+        'facebook',
+        'ville_id',
+        'user_id',
+        'souscategorie_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function villes()
+    {
+        return $this->belongsTo(Ville::class, 'ville_id');
+    }
+
+    public function souscat()
+    {
+        return $this->belongsTo(SousCategorie::class, 'souscategorie_id');
+    }
 }
